f9a38a84eaaad8f15fac68462a80acd3e677c07e
Laura Anacleto <laur4kauany@gmail.com>
2025-12-09 08:12:15 -0300

    Arquivos de POO

---
+++ POO/Pilares/Encapsulamento/Program.cs
@@ -0,0 +1,21 @@
+﻿using Encapsulamento;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Carro c1 = new Carro();
+
+        c1.DefinirMarca("BMW");
+        c1.DefinirModelo("Hatch");
+
+        c1.Acelerar(50);
+        c1.Acelerar(20);
+        c1.Frear(30);
+        c1.Frear(50);
+
+        Console.WriteLine($"Marca do carro:  {c1.ObterMarca()}");
+        Console.WriteLine($"Modelo do carro: {c1.ObterModelo()}");
+        Console.WriteLine($"Velocidade atual: {c1.ObterVelocidade()} km/h");
+    }
+}
